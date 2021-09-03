f7c24b4b39b83fac93d096fd688a61b80a6d4979
timothyshen <timothyshen1@hotmail.com>
2021-09-03 22:04:00 +0100

    transformed to vite

--- app/product/urls.py
+++ app/product/urls.py
@@ -16,7 +16,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("topup/", TopUpView.as_view(), name='get_top_up'),
     path("subscription/", SubscriptionView.as_view(), name='get_subscription'),
-    path("user_profile/", UserProfileView.as_view(), name='User_profile'),
+    path("user_profile/", UserProfileView.as_view({'get': 'list'}), name='User_profile'),
     # path('account/upgrade_plan/', upgrade_account, name='upgrade_plan'),
     # path('account/cancel_plan/', cancel_plan, name='cancel_plan'),
 ]
--- app/product/views.py
+++ app/product/views.py
@@ -4,6 +4,8 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
 from TheBookshelf import settings
 from .models import Top_up_item, Subscription_Plan, User_profile
@@ -24,13 +26,22 @@ class SubscriptionView(ListAPIView):
     permission_classes = [AllowAny]
 
 
-class UserProfileView(ListAPIView):
+class UserProfileView(ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = UserProfileSerializer
+    queryset = User_profile.objects.all()
 
-    def get_queryset(self):
-        return User_profile.objects.get_or_create(plan_id=3, user_id=self.request.user.id)
-
+    def list(self, request, *args, **kwargs):
+        user_profile, created = User_profile.objects.get_or_create(plan_id=3, user_id=self.request.user.id)
+        print(user_profile)
+        print(created)
+        if user_profile:
+            print('exist')
+            user_profile = User_profile.objects.filter(user_id=self.request.user.id).first()
+            serializer = UserProfileSerializer(user_profile)
+            return Response(serializer.data)
+        serializer = UserProfileSerializer(created)
+        return Response(serializer.data)
 
 @api_view(['POST'])
 def cancel_plan(request):
